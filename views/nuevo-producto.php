32708631f852beb375478714d3483a8520a80b3b
YovanaAmelia <auccatomacruzyovana@gmail.com>
2024-10-17 11:29:05 -0500

    actul

---
+++ views/nuevo-producto.php
@@ -0,0 +1,45 @@
+
+<form action="">  
+<div>
+        <label for=""> Codigo </label> <br>
+            <input type="text" placeholder=""><br>
+    </div>
+    <div>
+        <label for=""> Nombre </label><br>
+            <input type="text" placeholder=""><br>
+    </div>
+    <div>
+        <label for=""> Detalle </label><br>
+            <input type="text" placeholder=""><br>
+    </div>
+    <div>
+        <label for=""> Precio </label><br>
+            <input type="text" placeholder=""><br>
+    </div>
+    <div>
+       
+        <label for=""> Stock Inicial  </label><br>
+            <input type="text" placeholder=""><br>
+    </div>
+    <div>
+        <label for=""> categoria </label><br>
+            <input type="number" placeholder=""><br>
+    </div>
+    <div>
+        <label for=""> Fecha Vencimiento </label><br>
+            <input type="text" placeholder=""><br>
+    </div>
+    <div>
+        <label for=""> Imagen </label><br>
+            <input type="text" placeholder=""><br>
+    </div>
+    <div>
+        <label for=""> Proveedor </label><br>
+            <input type="number" placeholder=""><br>
+    </div>
+    <br>
+    <button type="submit">Registrar</button><br>
+  
+</form>
+<script src="<?php echo BASE_URL;?>views/js/functions_producto.js"></script>
+
